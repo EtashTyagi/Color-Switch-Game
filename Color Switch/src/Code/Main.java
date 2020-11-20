02c893ecb85aad0cddffc811d263fa2cd0813c78
EtashTyagi <51668955+EtashTyagi@users.noreply.github.com>
2020-11-21 00:02:27 +0530

    Mostly Final Main
    May Change Stage width and height

--- Color Switch/src/Code/Main.java
+++ Color Switch/src/Code/Main.java
@@ -1,14 +1,18 @@
 package Code;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 // DONE MAIN
 public class Main extends Application {
-    final public static int STAGE_WIDTH = 800;
-    final public static int STAGE_HEIGHT = 500;
+    final public static int STAGE_WIDTH = 500;
+    final public static int STAGE_HEIGHT = 600;
     @Override
     public void start(Stage primaryStage) throws Exception{
         openMainMenu(primaryStage);
@@ -16,10 +20,15 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    private static void openMainMenu(Stage primaryStage) {
-        Parent mainMenu = new MainMenu(primaryStage);
+    private void openMainMenu(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+        Parent mainMenu = loader.load();
+        MainMenu controller = loader.getController();
+        controller.setMainStage(primaryStage);
+        primaryStage.initStyle(StageStyle.UNDECORATED); //REMOVE TITLE BAR
         primaryStage.setTitle("Colour Switch");
         primaryStage.setScene(new Scene(mainMenu, STAGE_WIDTH, STAGE_HEIGHT));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
