1ccb35372aa9386e0a50b564f6f032bca112a413
EtashTyagi <51668955+EtashTyagi@users.noreply.github.com>
2020-11-24 23:43:29 +0530

    Added Animation To Lines

--- Color Switch/src/Code/LinesObstacles.java
+++ Color Switch/src/Code/LinesObstacles.java
@@ -1,18 +1,56 @@
 package Code;
 
-import javafx.scene.paint.Color;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 
 public class LinesObstacles extends Obstacle{
-    private double speed;
+    @FXML private Rectangle first;
+    @FXML private Rectangle second;
+    @FXML private Rectangle third;
+    @FXML private Rectangle fourth;
+    @FXML private Rectangle fifth;
+    private final double defaultLength = 125;
+    private double speed = 2.5;
 
-
+    @FXML private void initialize() {
+        doMovement();
+    }
     //TODO: Check For Collision
     @Override
     public boolean hasCollidedWithBall(Ball ball) {
         return false;
     }
-    //TODO: Implement translation
     @Override
     void doMovement() {
+        Thread animationThread = new Thread(() ->
+        {
+            while (true) {
+                Platform.runLater(() ->
+                {
+                    if (first.getWidth() <= 0) {
+                        fifth.setWidth(0);
+                        first.setFill(second.getFill());
+                        first.setWidth(defaultLength);
+                        Paint fifthColor = fifth.getFill();
+                        fifth.setFill(first.getFill());
+                        first.setFill(second.getFill());
+                        second.setFill(third.getFill());
+                        third.setFill(fourth.getFill());
+                        fourth.setFill(fifthColor);
+                    }
+
+                    first.setWidth(first.getWidth() - speed);
+                    fifth.setWidth(fifth.getWidth() + speed);
+                });
+                try {
+                    Thread.sleep(Main.UPDATE_IN);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        animationThread.start();
     }
 }
